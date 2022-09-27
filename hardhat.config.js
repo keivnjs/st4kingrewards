6e64e984a83c030166a3f7f983b8aa906e5d0bd0
kevinjs <kjsurtan@gmail.com>
2022-09-28 02:38:14 +0700

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/bdbe66fbcc554f12b7a2fd9cdfec6598',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
